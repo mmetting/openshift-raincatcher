335bba1cac1821d28c37dbe10e1c053567823447
Austin Cunningham <aucunnin@redhat.com>
2017-11-23 16:39:32 +0000

    add a deploy scirpt

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# start cluster only if installed 
+oc cluster up
+
+# login in to cluster with username and password
+oc login -u developer -p developer
+
+# create project called raincatcher
+oc new-project raincatcher --display-name="Raincatcher"
+
+# change to the raincatcher project
+oc project raincatcher
+
+# deploy the file
+oc create -f raincatcher.json
